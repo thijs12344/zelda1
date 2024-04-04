a5c6831c616afe20b8064de2aef7de402c0fe84c
monkeshrek <d303802@edu.curio.nl>
2024-04-04 09:17:34 +0200

    jayden
    jayden in spel gezet

--- zelda 1/Assets/Scenes/playermove.cs
+++ zelda 1/Assets/Scenes/playermove.cs
@@ -2,16 +2,21 @@ using UnityEngine;
 
 public class zelda1 : MonoBehaviour
 {
+    public float speed = 5f; // Adjust the speed as needed
 
-    public float speed = 0.1f;
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        float xDirection = Input.GetAxis("Horizontal");
-        float zDirection = Input.GetAxis("Vertical");
+        // Get input from Unity's Input system
+        float xDirection = Input.GetAxis("horizontal");
+        float zDirection = Input.GetAxis("vertical");
 
+        // Create a movement vector based on input
         Vector3 moveDirection = new Vector3(xDirection, 0.0f, zDirection);
 
-        transform.position += moveDirection * speed;
+        // Normalize the movement vector to maintain constant speed diagonally
+        moveDirection.Normalize();
+
+        // Move the object based on input and speed
+        transform.position += moveDirection * speed * Time.fixedDeltaTime;
     }
 }
